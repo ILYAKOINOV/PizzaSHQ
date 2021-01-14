244ad3911a5d89af4df48cd58b05d299c9a48d35
ILYAKOINOV <Aspiral1984@gmail.com>
2021-01-14 15:34:31 +0300

    Change app.js

--- app.rb
+++ app.rb
@@ -1,4 +1,3 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
@@ -7,7 +6,7 @@ get '/' do
 	erb :index			
 end
 
-get '/about' do
+get '/about' do  
 	erb :about
 end
 
